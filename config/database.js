aaeb4cc95ffe8bac3abc5f2021f403c429a944da
shivamkrishnaa <shivamkrishna6@gmail.com>
2019-05-09 11:58:30 +0530

    make it bettser

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 ////////////////////////////Postavke baze podataka///////////////////////////////
 
 module.exports={
-    database:'mongodb+srv://shivam:shivamkrishna@cluster0-8cp18.mongodb.net/test?retryWrites=true',//url adresa baze
+    database:'mongodb://rent:Sickomode69@ds153766.mlab.com:53766/rent',//url adresa baze
     secret:'mojatajna'  //tajni kod koji passport koristi za kreiranje tokena
     //obavezno mora sadrzati imena casopisa kao sto su: moja tajna, moja sudbina... 
 }
